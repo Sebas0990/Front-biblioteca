80fa05daf4d54bdf40a1f805f47171ab628677bb
User <User@CPU-DE-SEBAS>
2022-05-28 20:28:02 -0500

    Agregar archivos de proyecto.

---
+++ Biblioteca/Data/Pelicula.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Biblioteca.Data
+{
+    public class Pelicula
+    {
+        [Key]
+        public int IdPelicula { get; set; }
+        public string Titulo { get; set; }
+        public string Productora { get; set; }
+        public string Director { get; set; }
+        public int Duracion { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
